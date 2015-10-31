723df1e60f0435c132ea54ec6d4a6f287e11a854
sam schonstal <sam@schonstal.com>
2015-10-30 23:10:36 -0700

    top 5 by category

--- lib/parser.rb
+++ lib/parser.rb
@@ -4,6 +4,12 @@ class Parser
    attr_accessor :data
 
    def initialize(fileStream)
-       @data =  JSON.parse(fileStream.read)
+       @data = JSON.parse(fileStream.read)
+   end
+
+   def topInCategory(top)
+      data.group_by{|item| item["type"]}
+         .map{|category, data| data.sort_by {|item| -item["price"] }
+         .slice(0,top)}
    end
 end
--- specs/parser_spec.rb
+++ specs/parser_spec.rb
@@ -13,7 +13,29 @@ describe "parser" do
       end
 
       it "should load the test file" do
-         expect(@parser.data.length).to equal(3)
+         expect(@parser.data.length).to equal(19)
+      end
+   end
+
+   describe "topInCategory" do
+      before :each do
+         @top = @parser.topInCategory(5)
+      end
+
+      it "should return 3 arrays of 5 items" do
+         expect(@top[0].length).to equal(5)
+         expect(@top[1].length).to equal(5)
+         expect(@top[2].length).to equal(5)
+      end
+
+      it "should have most expensive item at head of array" do
+         expect(@top[0][0]["price"]).to equal(16.99)
+         expect(@top[1][0]["price"]).to equal(16.99)
+         expect(@top[2][0]["price"]).to equal(16.99)
+      end
+
+      it "all items should be more than 12.99" do
+         @top.each { |category| category.each{ |item| expect(item["price"]).to be > 12.98}}
       end
    end
 end
